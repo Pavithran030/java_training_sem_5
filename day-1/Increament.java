8cc271a18de93fa445583c468a5e0c27d1070fda
Pavithran030 <techpavithran18@gmail.com>
2025-09-15 15:30:42 +0530

    update the error

--- day-1/Increament.java
+++ day-1/Increament.java
@@ -3,12 +3,13 @@ import java.util.Scanner;
 
 public class Increament {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
+        try(Scanner sc=new Scanner(System.in)){
         System.out.print("Enter the number to Understand the Increment Operation : ");
         int a=sc.nextInt();
         System.out.println(a++);
         System.out.println(a--);
         System.out.println(++a);
         System.out.println(--a);
+        }
     }
 }
