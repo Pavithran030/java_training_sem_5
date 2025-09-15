5d0c4764db86eebbecd2316fa24b179731d1e815
Pavithran030 <techpavithran18@gmail.com>
2025-09-15 14:17:13 +0530

    operators

---
+++ day-1/Operations.java
@@ -0,0 +1,9 @@
+public class Operations {
+    public static void main(String[] args) {
+        int a =20;
+        float f=30.5f;
+        char c='A';
+        System.out.println(a+f);
+        System.out.println(a+c);
+    }
+}
