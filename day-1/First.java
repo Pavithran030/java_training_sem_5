9278e043b0901186adcfdf1d5edf1ea3722c3bfa
Pavithran030 <techpavithran18@gmail.com>
2025-09-15 11:54:16 +0530

    initial-commit

--- day-1/First.java
+++ day-1/First.java
@@ -1,5 +1,12 @@
+
+//variable declaration
+
 public class First {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Hacking is the way of crossing the Limitations");
+        int a=2;
+        float b=20.56f;
+        System.out.println(a);
+        System.out.println(b);
     }
 }
