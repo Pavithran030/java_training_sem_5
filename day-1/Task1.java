71cca1892bcb76309fed04b27195c7aa09e49378
Pavithran030 <techpavithran18@gmail.com>
2025-09-15 14:28:38 +0530

    operationn-task-1

---
+++ day-1/Task1.java
@@ -0,0 +1,14 @@
+public class Task1 {
+    public static void main(String[] args) {
+        int a=20,b=40;
+        double d=300.078;
+        int c='H';
+        System.out.println(a+b);
+        System.out.println(a+d);
+        System.out.println(a/b);
+        System.out.println(a/d);
+        System.out.println(c+c);
+        System.out.println(c);
+    }
+    
+}
