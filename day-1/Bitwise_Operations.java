682724bf5a5e177b2f4176833ecfc5d5d2dcb8fd
Pavithran030 <techpavithran18@gmail.com>
2025-09-15 15:33:31 +0530

    bitwise-operations

---
+++ day-1/Bitwise_Operations.java
@@ -0,0 +1,9 @@
+public class Bitwise_Operations {
+    public static void main(String[] args) {
+        System.out.println(4<<1);
+        System.out.println(4<<2);
+        System.out.println(4>>1);
+        System.out.println(4>>2);
+    }
+    
+}
