8ff8abb0677a5fb1a1a5f4617a564897d5b16d06
Pavithran030 <techpavithran18@gmail.com>
2025-09-16 10:18:40 +0530

    DT-Problem -1

---
+++ day-2/Primitive_DT.java
@@ -0,0 +1,22 @@
+public class Primitive_DT {
+    public static void main(String[] args) {
+        byte by=127;
+        short sr=150;
+        int num=5;
+        long l1=102345;
+        float f1=30.5f;
+        double d1=40.890;
+        char c1='J';
+        boolean b1=false;
+        System.out.println("Byte value: "+by);
+        System.out.println("Short value: "+sr);
+        System.out.println("Integer value: "+num);
+        System.out.println("Long value: "+l1);
+        System.out.println("Float value :"+f1);
+        System.out.println("Double value :"+d1);
+        System.out.println("char value :"+c1);
+        System.out.println("Boolean value :"+b1);
+               
+    }
+    
+}
