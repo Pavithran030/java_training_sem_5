b91f7172a19b0cb2efa6d61d3fa4fe4f4a2815c1
Pavithran030 <techpavithran18@gmail.com>
2025-09-16 10:04:11 +0530

    problem-2-practice

---
+++ day-2/Circle.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Circle {
+    public static void main(String[] args) {
+        try(Scanner sc =new Scanner(System.in)){
+            System.out.print("Enter the Radius of the Circle : ");
+            int ra=sc.nextInt();
+            System.out.println("The Radius of the Circle is : "+(3.14*(ra*ra)));
+        }
+
+    }
+    
+}
