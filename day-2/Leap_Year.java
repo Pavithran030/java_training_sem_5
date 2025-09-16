8d1c2da0bdc81d7d4a5c81840aa7b6d81c1d156e
Pavithran030 <techpavithran18@gmail.com>
2025-09-16 11:59:22 +0530

    condition statement Problem-1

---
+++ day-2/Leap_Year.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Leap_Year {
+    public static void main(String[] args) {
+        try(Scanner inp=new Scanner(System.in)){
+            System.out.print("Enter the Year to find Leap Year or Not : ");
+            int year=inp.nextInt();
+            if(year%4==0 &&(year%400==0 || year%100!=0)){
+                System.out.println(year+ " is a Leap Year");
+            }
+            else {
+                System.out.println(year+ " is Not a Leap Year");
+            }
+        }
+    }
+    
+}
