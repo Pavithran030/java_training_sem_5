b0c6f022fe271fb2d1362e2cc8124d634824eb5a
Pavithran030 <techpavithran18@gmail.com>
2025-09-16 12:27:27 +0530

    condition statement problem-3

---
+++ day-2/Nested_If.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Nested_If {
+    public static void main(String[] args) {
+        try(Scanner inp=new Scanner(System.in)){
+            System.out.print("Ente Your age : ");
+            int age=inp.nextInt();
+
+            if(age>=18) {
+                System.out.println("Eligilbe");
+                if(age>=19 && age<=24) System.out.println("Adult vote");
+                if(age>=25 && age <=59) System.out.println("Senior vote");
+                if(age>=60) System.out.println("Senior citizen vote");
+
+            }
+            if(age<18) System.out.println("Not Eligible");
+        }
+    }
+    
+}
