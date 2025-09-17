afd38b5646bc9888caf2a50e345b4bdd52668912
Pavithran030 <techpavithran18@gmail.com>
2025-09-17 12:12:26 +0530

    Array task-2

---
+++ day-3/Reverrse_Array.java
@@ -0,0 +1,9 @@
+public class Reverrse_Array {
+    public static void main(String[] args) {
+        int[] lar={2,5,7,9,3,4,6,20};
+        for(int i=lar.length-1;i>0;i--){ 
+            System.out.print(lar[i]+" ");
+        }
+    }
+    
+}
