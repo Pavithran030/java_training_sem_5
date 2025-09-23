21b64caae16c3178d504d0e6d58768c1c34c6a37
Pavithran030 <techpavithran18@gmail.com>
2025-09-23 16:09:44 +0530

    Methods Intro

---
+++ day-5/Methods.java
@@ -0,0 +1,12 @@
+
+
+public class Methods {
+    public static void main(String[] args) {
+        // Methods c=new Methods();
+        car();
+    }
+    static void car(){
+        System.out.println("Its a Ferrahi");
+    }
+    
+}
