28ce7359f0a9615a172fc048a7e30156cef46f58
Pavithran030 <techpavithran18@gmail.com>
2025-09-29 10:45:07 +0530

    This Keyword

---
+++ day-7/This.java
@@ -0,0 +1,25 @@
+public class This {
+    public static void main(String[] args) {
+        Man m1=new Man("Ben-10");
+        m1.display();
+    }
+    
+}
+
+class Man{
+    String a;
+
+    Man(String b) {
+        a=b;
+    }
+    void display(){
+        System.out.print(a);
+        this.show();
+    }
+
+    void show(){
+        System.out.println(" "+"Ultimate Alien....");
+    }
+
+    
+}
