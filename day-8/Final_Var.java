849c6362f0707458bd1b92ae94ceb5503c3fa90b
Pavithran030 <techpavithran18@gmail.com>
2025-09-30 11:22:55 +0530

    Final variable

---
+++ day-8/Final_Var.java
@@ -0,0 +1,15 @@
+class Fin{
+    String sen="Mango";
+    void show(){
+        System.out.println(sen);
+    }
+}
+
+public class Final_Var {
+    public static void main(String[] args) {
+        Fin f=new Fin();
+        f.sen="Apple";
+        f.show();
+        
+    }
+}
