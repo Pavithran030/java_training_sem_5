d2370f325bcf545375f2e12ff9295e8ad442f45b
Pavithran030 <techpavithran18@gmail.com>
2025-09-30 18:45:18 +0530

    Final Method

---
+++ day-8/Final_Method.java
@@ -0,0 +1,24 @@
+class A{
+    final void show(int a ){
+        System.out.println(a);
+    }
+
+    void show(){
+        System.out.println("..............");
+    }
+}
+
+class B extends A{
+    void show(){
+        
+    }
+}
+
+public class Final_Method {
+    public static void main(String[] args) {
+        A a1=new A();
+        a1.show(10);
+        a1.show();
+        
+    }
+}
