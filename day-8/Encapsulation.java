6110010e7cebf67c0cd0bebf75beba246f1bbfc6
Pavithran030 <techpavithran18@gmail.com>
2025-09-30 09:51:16 +0530

    Encapsulation-Private

---
+++ day-8/Encapsulation.java
@@ -0,0 +1,15 @@
+class Mango{
+    private final int num=7;
+    void salem(){
+        System.out.println("It is a Encapsulation Class...");
+        System.out.println(num);
+    }
+}
+
+
+public class Encapsulation {
+    public static void main(String[] args) {
+        Mango m=new Mango();
+        m.salem();        
+    }
+}
