52f5998cfac0c8a45b4961cf1522885bae3c63b5
Pavithran030 <techpavithran18@gmail.com>
2025-10-06 12:08:38 +0530

    Exception

--- day-9/Except.java
+++ day-9/Except.java
@@ -1,11 +1,13 @@
-public class Exception {
+public class Except {
     public static void main(String[] args) {
         int nu=35;
         int de=0;
         try {
             int re=nu/de;
             System.out.println(re);
-        } catch (ArithmeticException e) {
+            // int[] arr=new int[2];
+            // arr[5]=10;
+        } catch (Exception e) {
             System.out.println("The Exception Happened here is "+e);
         }
         finally{
