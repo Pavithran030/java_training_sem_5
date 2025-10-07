e038718dd7365b911daede09c4eb7a3027df4555
Pavithran030 <techpavithran18@gmail.com>
2025-10-07 15:51:48 +0530

    Single Linkledlist all Operations

--- day-10/list_single/Single_LinkedList.java
+++ day-10/list_single/Single_LinkedList.java
@@ -19,7 +19,17 @@ class List{
         head=f;
     }
 
-    // To Insert a Data into a linked list
+    //TO INSERT DATA AT THE LAST....
+
+    void atlast(int num){
+
+        Node l=new Node(num);
+        tail.next=l;
+        tail=l;
+
+    }
+
+    // TO INSERT A DATA INTO A LINKED LIST
 
     void insert(int data){
         Node da=new Node(data);   
@@ -36,7 +46,91 @@ class List{
         
     }
 
-    // To display the data in from the linked list..
+    // INSERT DATA AT THE SPECIFIC LOCATION
+
+    void location(int ind,int val){
+
+        Node spec=head;
+
+        Node s=new Node(val);
+
+        for(int i=0;i<ind-1;i++){
+            spec=spec.next;
+        }
+
+        s.next=spec.next;
+        spec.next=s;
+
+    }
+
+    // INSERT DATA AFTER THE SPECIFIC VALUE
+
+    void place(int pl,int val){
+        Node temp=head;
+
+        Node p=new Node(val);
+
+        while(temp.data!=pl){
+            temp=temp.next;
+        }
+        p.next=temp.next;
+        temp.next=p;
+
+
+    }
+
+    //DELETE AT THE FIRST...
+
+    void deletefirst(){
+
+        Node temp=head;
+        if(temp==head && temp==tail){
+            head=null;
+            tail=null;
+            return;
+        }
+
+        head=temp.next;
+
+    }
+
+    //DELETE AT THE LAST
+
+    void deletelast(){
+        Node temp=head;
+
+        if(head==null) {
+            System.out.println("The list is Empty....Can't able to delete Elements....");
+            return;
+        }
+
+        if(head==tail){
+            head=null;
+            tail=null;
+            return;
+        }
+        
+        while(temp.next!=tail){
+            temp=temp.next;
+        }
+        temp.next=null;
+        tail=temp;
+    }
+
+    //DELETE AT THE SPECIFIC POSITION..
+
+    void posdelete(int ind){
+        Node pos=head;
+
+        for(int i=0;i<ind-1;i++){
+            pos=pos.next;
+        }
+
+        pos.next=pos.next.next;
+        // pos.next=null;
+    }
+
+    // TO DISPLAY THE DATA IN FROM THE LINKED LIST..
 
     void print(){
         Node temp=head;
@@ -53,18 +147,38 @@ class List{
             }
             
         }
+        System.out.println();
     }
+
 }
 public class Single_LinkedList {
 
     public static void main(String[] args) {
         List ls=new List();
-        ls.insert(10);
-        ls.insert(40);
-        ls.insert(60);
-        ls.insert(80);
-        ls.atfirst(0);
+
+        for(int i=80;i>=0;i-=20){
+            ls.insert(i);
+        }
+        System.out.println("The Linked List After Insert the Initial Elements...");
         ls.print();
+        System.out.println();
+
+        ls.atfirst(22);
+        ls.atlast(100);
+        ls.location(2, 30);
+        ls.place(60, 65);
+        System.out.println("The Linked List After Performing Insertion at the first,last and Specific Location....");
+        ls.print();
+        System.out.println();
+
+        
+        ls.deletefirst();
+        ls.deletelast();
+        ls.posdelete(2);
+        System.out.println("The Linked List After Performing Deletion at the first,last and Specific Location....");
+        ls.print();
+        System.out.println();
+
     }
     
 }
