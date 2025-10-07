a4cb1400398f8daf7bc8ae24790d75c7a32b81e1
Redhat <redhat@Redhat.redhat_kali.com>
2025-10-07 21:30:53 +0530

    Double Linkedlist

---
+++ day-10/list_double/Double_LinkedList.java
@@ -0,0 +1,18 @@
+
+class Node{
+    int data;
+    Node next;
+    Node pre;
+
+    Node(int data){
+        this.data=data;
+    }
+}
+
+
+public class Double_LinkedList {
+    public static void main(String[] args) {
+        System.out.println("Hai");
+    }
+    
+}
